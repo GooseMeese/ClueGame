395deaa1cc1df7a0f7070fee493e2c863a48b9d0
GooseMeese <ahansen1@mines.edu>
2023-10-06 16:24:47 -0600

    Finished Adjacency Tests

--- Clue Paths 1/src/tests/BoardTestsExp.java
+++ Clue Paths 1/src/tests/BoardTestsExp.java
@@ -14,12 +14,14 @@ class BoardTestsExp {
 	}
 	
 	
+	
 	/*
 	 * Test adjacencies for different player locations
 	 * Test centers and edges
 	 */
+	// Tests for adjacencies in the 0,0 corner cell
 	@Test
-	public void testAdjacency() {
+	public void testAdjacency1() {
 		TestBoardCell cell = board.getCell(0, 0);
 		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
@@ -28,13 +30,62 @@ class BoardTestsExp {
 	}
 	
 	
+	// Tests for adjacencies in the 3,3 corner cell
+	@Test
+	public void testAdjacency2() {
+		TestBoardCell cell = board.getCell(3, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(3, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 3)));
+		Assert.assertEquals(2, testList.size());
+	}
+	
+	
+	// Tests for adjacencies in the 1,1 middle cell
+	@Test
+	public void testAdjacency3() {
+		TestBoardCell cell = board.getCell(1, 1);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 1)));
+		Assert.assertEquals(4, testList.size());
+	}
+	
+	
+	// Tests for adjacencies in the 1,3 edge cell
+	@Test
+	public void testAdjacency4() {
+		TestBoardCell cell = board.getCell(1, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 3)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 3)));
+		Assert.assertEquals(3, testList.size());
+	}
+
+	
+	// Tests for adjacencies in the 2,0 edge cell
+	@Test
+	public void testAdjacency5() {
+		TestBoardCell cell = board.getCell(2, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 1)));
+		Assert.assertTrue(testList.contains(board.getCell(3, 0)));
+		Assert.assertEquals(3, testList.size());
+	}
+	
+	
+	
 	
 	
 	/*
 	 * Test targets with several rolls and start locations
 	 */
 	@Test
-	public void testTargetsNormal() {
+	public void testTargetsNormal1() {
 		TestBoardCell cell = board.getCell(0, 0);
 		board.calcTargets(cell, 3);
 		Set<TestBoardCell> targets = board.getTargets();
@@ -50,11 +101,54 @@ class BoardTestsExp {
 	
 	
 	
+	
+	/*
+	 * Test for targets when a room is involved
+	 */
+	@Test
+	public void testTargetsRoom1() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+	
+	
+	
+	
+	
+	/*
+	 * Test for targets when an interfering occupied space is involved
+	 */
+	@Test
+	public void testTargetsOccupied1() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+	
+	
+	
+	
+	
 	/*
 	 * Test a situation where they may be a cell occupied by a room and another that is occupied by an opponent
 	 */
 	@Test
-	public void testTargetsMixed() {
+	public void testTargetsMixed1() {
 		board.getCell(0, 2).setOccupied(true);
 		board.getCell(0, 2).setIsRoom(true);
 		TestBoardCell cell = board.getCell(0, 3);
