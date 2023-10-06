52bccacf01557d50a209a9f0fb8d6958f595f938
GooseMeese <ahansen1@mines.edu>
2023-10-06 14:56:58 -0600

    Test Skeleton Created

---
+++ Clue Paths 1/src/experiment/TestBoard.java
@@ -0,0 +1,29 @@
+package experiment;
+
+import java.util.*;
+
+public class TestBoard {
+	
+	// Empty constructor
+	public TestBoard() {
+		super();
+	}
+	
+	// Calculates targets for a move from startCell of length pathlength.
+	public void calcTargets(TestBoardCell startCell, int pathLength) {
+		
+	}
+	
+	// Returns the cell from the board at row, col
+	public TestBoardCell getCell(int row, int col) {
+		return null;
+	}
+	
+	// Gets the targets last created by calcTargets()
+	public Set<TestBoardCell> getTargets(){
+		return null;
+	}
+	
+	
+	
+}
---
+++ Clue Paths 1/src/experiment/TestBoardCell.java
@@ -0,0 +1,32 @@
+package experiment;
+
+import java.util.*;
+
+public class TestBoardCell {
+	
+	// Constructor with row and col inputs
+	public TestBoardCell(int row, int col) {
+		super();
+	}
+	
+	// Adds an adjacent cell to this cells adjacency list
+	public void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+	// Returns the adjacency list for the cell
+	public Set<TestBoardCell> getAdjList(){
+		return null;
+	}
+	
+	public void setIsRoom(boolean cell) {
+		return;
+	}
+	
+	// Setter for indicating if a cell is occupied by another player
+	public void setOccupied(boolean cell) {
+		return;
+	}
+
+	
+}
