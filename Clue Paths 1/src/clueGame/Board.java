1c30de6872a8abeaaaf03b2edee3ac6c1aa47f15
GooseMeese <ahansen1@mines.edu>
2023-10-10 22:15:07 -0600

    fixed board class

--- Clue Paths 1/src/clueGame/Board.java
+++ Clue Paths 1/src/clueGame/Board.java
@@ -8,25 +8,37 @@ public class Board {
 	private String layoutConfigFile;
 	private String setupConfigFile;
 	private Map<Character, Room> roomMap;
-	private static Board theInstance;
-	
-	// Default Constructor
-	public Board() {
-		super();
+
+	/*
+	 * variable and methods used for singleton pattern
+	 */
+	private static Board theInstance = new Board();
+
+	// constructor is private to ensure only one can be created
+	private Board() {
+		super() ;
 	}
-	
-	
+
+	// this method returns the only Board
+	public static Board getInstance() {
+		return theInstance;
+	}
+
+
+	/*
+	 * initialize the board (since we are using singleton pattern)
+	 */
 	public void initialize() {
 		// Empty
 	}
-	
+
 	public void loadSetupConfig(){
 		// Empty
 	}
-	
+
 	public void loadLayoutConfig(){
 		// Empty
 	}
-	
+
 
 }
